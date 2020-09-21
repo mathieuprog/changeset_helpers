49cf33e90c762f2e951de28a7940861194f7a787
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2020-09-21 10:48:36 +0200

    Remove bang from `change_assoc!`

--- lib/changeset_helpers.ex
+++ lib/changeset_helpers.ex
@@ -3,52 +3,52 @@ defmodule ChangesetHelpers do
   Documentation for `ChangesetHelpers`.
   """
 
-  def change_assoc!(struct_or_changeset, keys, changes \\ %{}) do
-    changed_assoc = do_change_assoc!(struct_or_changeset, keys, changes)
+  def change_assoc(struct_or_changeset, keys, changes \\ %{}) do
+    changed_assoc = do_change_assoc(struct_or_changeset, keys, changes)
 
     {put_assoc(struct_or_changeset |> Ecto.Changeset.change(), keys, changed_assoc), changed_assoc}
   end
 
-  defp do_change_assoc!(%Ecto.Changeset{} = changeset, [key | []], changes) do
+  defp do_change_assoc(%Ecto.Changeset{} = changeset, [key | []], changes) do
     Map.get(changeset.changes, key, Map.fetch!(changeset.data, key))
-    |> do_change_assoc!(changes)
+    |> do_change_assoc(changes)
   end
 
-  defp do_change_assoc!(%{__meta__: _} = schema, [key | []], changes) do
+  defp do_change_assoc(%{__meta__: _} = schema, [key | []], changes) do
     Map.fetch!(schema, key)
-    |> do_change_assoc!(changes)
+    |> do_change_assoc(changes)
   end
 
-  defp do_change_assoc!(%Ecto.Changeset{} = changeset, [key | tail_keys], changes) do
+  defp do_change_assoc(%Ecto.Changeset{} = changeset, [key | tail_keys], changes) do
     Map.get(changeset.changes, key, Map.fetch!(changeset.data, key))
-    |> do_change_assoc!(tail_keys, changes)
+    |> do_change_assoc(tail_keys, changes)
   end
 
-  defp do_change_assoc!(%{__meta__: _} = schema, [key | tail_keys], changes) do
+  defp do_change_assoc(%{__meta__: _} = schema, [key | tail_keys], changes) do
     Map.fetch!(schema, key)
-    |> do_change_assoc!(tail_keys, changes)
+    |> do_change_assoc(tail_keys, changes)
   end
 
-  defp do_change_assoc!([], _changes), do: []
+  defp do_change_assoc([], _changes), do: []
 
-  defp do_change_assoc!([%{__meta__: _} = schema | tail], changes) do
-    [Ecto.Changeset.change(schema, changes) | do_change_assoc!(tail, changes)]
+  defp do_change_assoc([%{__meta__: _} = schema | tail], changes) do
+    [Ecto.Changeset.change(schema, changes) | do_change_assoc(tail, changes)]
   end
 
-  defp do_change_assoc!([%Ecto.Changeset{} = changeset | tail], changes) do
-    [Ecto.Changeset.change(changeset, changes) | do_change_assoc!(tail, changes)]
+  defp do_change_assoc([%Ecto.Changeset{} = changeset | tail], changes) do
+    [Ecto.Changeset.change(changeset, changes) | do_change_assoc(tail, changes)]
   end
 
-  defp do_change_assoc!(%{__meta__: _} = schema, changes) do
+  defp do_change_assoc(%{__meta__: _} = schema, changes) do
     Ecto.Changeset.change(schema, changes)
   end
 
-  defp do_change_assoc!(%Ecto.Changeset{} = changeset, changes) do
+  defp do_change_assoc(%Ecto.Changeset{} = changeset, changes) do
     Ecto.Changeset.change(changeset, changes)
   end
 
   def put_assoc(changeset, [key | []], fun) when is_function(fun) do
-    Ecto.Changeset.put_assoc(changeset, key, fun.(do_change_assoc!(changeset, [key], %{})))
+    Ecto.Changeset.put_assoc(changeset, key, fun.(do_change_assoc(changeset, [key], %{})))
   end
 
   def put_assoc(changeset, [key | []], value) do
@@ -56,7 +56,7 @@ defmodule ChangesetHelpers do
   end
 
   def put_assoc(changeset, [key | tail_keys], value) do
-    Ecto.Changeset.put_assoc(changeset, key, put_assoc(do_change_assoc!(changeset, [key], %{}), tail_keys, value))
+    Ecto.Changeset.put_assoc(changeset, key, put_assoc(do_change_assoc(changeset, [key], %{}), tail_keys, value))
   end
 
   def diff_field!(changeset1, changeset2, [key | []]) do
